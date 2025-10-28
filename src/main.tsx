7e2cd98cbff05dd2b00a47a4a897ab53eb36d257
Kavleen Sabharwal <kashishsabharwal@gmail.com>
2025-10-29 02:43:44 +0530

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { HashRouter, Routes, Route, Navigate, BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import './index.css'
 import Dashboard from './components/Dashboard/Dashboard'
@@ -10,7 +10,7 @@ import { store } from './store/store'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <Provider store={store}>
         <Routes>
           <Route path="/" element={<Navigate to="/dashboard" replace />} />
@@ -23,15 +23,15 @@ createRoot(document.getElementById('root')!).render(
             }
           />
           <Route
-            path="/editor/:id" 
+            path="/editor/:id"
             element={
               <ProtectedRoute>
                 <Editor />
               </ProtectedRoute>
-            } 
+            }
           />
         </Routes>
       </Provider>
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>,
 )
